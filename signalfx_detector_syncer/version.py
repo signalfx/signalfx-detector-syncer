68c669ab2a4acc7e2a14f0dca8f561b60998c5a9
Maxime Petazzoni <max@signalfx.com>
2017-04-11 14:36:04 -0700

    Version 0.8

--- signalfx_detector_syncer/version.py
+++ signalfx_detector_syncer/version.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2016 SignalFx, Inc. All rights reserved.
+# Copyright (C) 2016-2017 SignalFx, Inc. All rights reserved.
 
 name = 'signalfx-detector-syncer'
-version = '0.7'
+version = '0.8'
