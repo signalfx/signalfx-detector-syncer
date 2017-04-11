f30b32859b093d4874ac90adfd19c079145dc69a
Maxime Petazzoni <max@signalfx.com>
2017-04-11 14:34:40 -0700

    Add --version command-line flag

--- signalfx_detector_syncer/__main__.py
+++ signalfx_detector_syncer/__main__.py
@@ -7,29 +7,37 @@ import logging
 import signalfx
 import sys
 
-from . import syncer
+from . import syncer, version
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-t', '--token', required=True,
-                        help='Authentication token')
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('--version', action='store_true',
+                       help='show version information')
+    group.add_argument('-t', '--token',
+                       help='authentication token')
     parser.add_argument('--scope', help='An optional base scope')
     parser.add_argument('-a', '--api-endpoint',
                         help='SignalFx API endpoint')
     parser.add_argument('-n', '--dry-run', action='store_true',
-                        help='Dry-run mode, do not update detectors')
+                        help='dry-run mode, do not update detectors')
     parser.add_argument('-D', '--debug', action='store_const',
                         dest='loglevel',
                         const=logging.DEBUG, default=logging.WARNING,
-                        help='Enable debug logging')
+                        help='enable debug logging')
     parser.add_argument('-v', '--verbose', action='store_const',
                         dest='loglevel',
                         const=logging.INFO,
-                        help='Enable verbose logging')
-    parser.add_argument('directory', default='.', help='Source directory')
+                        help='enable verbose logging')
+    parser.add_argument('directory', nargs='?', default='.',
+                        help='Source directory')
 
     options = parser.parse_args()
+    if options.version:
+        print('{} v{}'.format(version.name, version.version))
+        return
+
     logging.basicConfig(stream=sys.stderr, level=options.loglevel,
                         format='%(asctime)s | %(levelname)8s | %(message)s')
     logging.getLogger('requests').setLevel(logging.WARNING)
